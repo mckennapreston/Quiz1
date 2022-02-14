6c4529dd1a78a3c248aaaa7a254135b1aa328fc2
Mckenna Preston <Mckenna_Preston1@baylor.edu>
2022-02-14 15:10:59 -0600

    quiz1

--- processStudents.py
+++ processStudents.py
@@ -1,4 +1,4 @@
-''' 
+""" 
 *   Professor B would like to know which of his student have a GPA below 3.0.
     To accomplish this, read the file - students.csv into the program. The program
     should evaluate the GPA to see if it is higher or lower than 3.0. If it is,
@@ -17,7 +17,7 @@ not required to use them but it is provided to help you work
 through the logic of the problem.
 
 
-'''
+"""
 
 
 import csv
@@ -25,54 +25,55 @@ import csv
 
 # create a file object to open the file in read mode
 
-
+students = open("students.csv", "r")
 
 # create a csv object from the file object
 
+students_file = csv.reader(students, delimiter=",")
 
-#skip the header row
+# skip the header row
 
+next(students_file)
 
-#create an outfile object for the pocessed record
-
-
-
-#create a new dictionary named 'student_dict'
+# create an outfile object for the pocessed record
 
 
-
-#use a loop to iterate through each row of the file
-
-    #check if the GPA is below 3.0. If so, write the record to the outfile
-    
-        
+def add_dict():
+    outfile = open("students.csv", "w")
 
 
+# create a new dictionary named 'student_dict'
 
-    # append the record to the dictionary with the student id as the Key
-    # and the value as the GPA
-    
+student_dict = {
+    "123456789": "3.00",
+    "124567890": "2.70",
+    "234567890": "3.50",
+    "345678901": "2.80",
+    "456789012": "3.20",
+    "567890123": "3.60",
+    "678901234": "3.30",
+    "789012345": "2.50",
+    "876543210": "4.00",
+    "890123456": "2.20",
+    "901234567": "3.80",
+}
 
+# use a loop to iterate through each row of the file
 
-
-
-
-#print the entire dictionary
-
-
-#Print the student id 
-
-
-#print out the corresponding GPA from the dictionary
-
-
-
-#close the outfile
-
+# check if the GPA is below 3.0. If so, write the record to the outfile
 
 
+# append the record to the dictionary with the student id as the Key
+# and the value as the GPA
 
 
+# print the entire dictionary
 
 
+# Print the student id
 
+
+# print out the corresponding GPA from the dictionary
+
+
+# close the outfile
