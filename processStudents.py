b3dcfcd1252b39f6193eed71fa5ee9ace7bff0b7
Mckenna Preston <Mckenna_Preston1@baylor.edu>
2022-02-14 15:59:40 -0600

    quiz1

--- processStudents.py
+++ processStudents.py
@@ -38,45 +38,57 @@ next(students_file)
 # create an outfile object for the pocessed record
 
 
-def add_dict():
-    outfile = open("students.csv", "w")
-
 
 # create a new dictionary named 'student_dict'
 
 student_dict = {
-    "123456789": "3.00",
-    "124567890": "2.70",
-    "234567890": "3.50",
-    "345678901": "2.80",
-    "456789012": "3.20",
-    "567890123": "3.60",
-    "678901234": "3.30",
-    "789012345": "2.50",
-    "876543210": "4.00",
-    "890123456": "2.20",
-    "901234567": "3.80",
+    "stud_id": [
+        ["firstname"],
+        ["lastname"],
+        ["major"],
+        ["classification"],
+        ["gpa"],
+    ]
 }
 
-for record in students_file 
-    StudentID = 
 
 # use a loop to iterate through each row of the file
+outfile = open("processedStudents.csv", "w")
 
+for l in student_dict["stud_id"]:
+    outfile.write(
+        (l["firstname"])
+        + ","
+        + (l["lastname"])
+        + ","
+        + (l["major"])
+        + ","
+        + (l["classification"])
+        + ","
+        + (l["gpa"])
+        + "\n"
+    )
+
+print(l)
 # check if the GPA is below 3.0. If so, write the record to the outfile
+
 
 
 # append the record to the dictionary with the student id as the Key
 # and the value as the GPA
 
+key = stud_id
+value = gpa
 
 # print the entire dictionary
-
+print(student_dict)
+print(type(student_dict))
 
 # Print the student id
-
+print(type(student_dict['stud_id']))
 
 # print out the corresponding GPA from the dictionary
-
+print(type(student_dict['gpa']))
 
 # close the outfile
+outfile.close()
